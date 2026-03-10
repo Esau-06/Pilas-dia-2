bfbb5c27e4250166d0015034933135177fcd03db
Sala K 405 <salak405@salasfr.itm.edu.co>
2026-03-10 09:16:20 -0500

    Pilas

--- main.java
+++ main.java
@@ -1,10 +1,12 @@
 import java.util.Scanner;
+import java.util.Stack;
 
 public class main {
     public static void main(String[] args) {
         boolean seguir = true;
         Scanner sc = new Scanner(System.in);
         utilities u = new utilities();
+        Stack<Integer> pila = new Stack<>();
 
         while (seguir) {
             System.out.println("Bienvenidos a la clase de pilas vamos a mirar un CRUD");
@@ -15,6 +17,27 @@ public class main {
             System.out.println("4) Eliminar");
             System.out.println("5) Salir");
             int opt = u.ValidarEntero(sc);
+            switch (opt) {
+                case 1: 
+                    pila = u.LlenarPila(pila);
+                    break;
+                case 2: 
+                    u.MostrarPila(pila);
+                    break;
+                    case 3: 
+                    System.out.println("Pagina en mantenimiento");
+                    break;
+                    case 4: 
+                    System.out.println("Pagina en mantenimiento");
+                    break;
+                    case 5: 
+                    System.out.println("Hasta luego");
+                    seguir = false;
+                    break;
+                default:
+                    System.out.println("Opcion no valida");
+                    break;
+            }
             System.out.println(opt);
 
         }
--- utilities.java
+++ utilities.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
+import java.util.Stack;
+
 
 public class utilities {
+    Scanner sc = new Scanner(System.in);
     public int ValidarEntero(Scanner sc)
     {
         while (!sc.hasNextInt()) {
@@ -8,10 +11,9 @@ public class utilities {
             sc.nextLine();
         }
         return sc.nextInt();
-
-
-
     }
+
+
     public Double ValidarDecimal(Scanner sc)
     {
         while (!sc.hasNextDouble()) {
@@ -21,5 +23,35 @@ public class utilities {
         return sc.nextDouble();
     }
 
-    public Stack<Integer> LlenarPila(Stack)
+
+    public int ValidarRango(int n1, int n2, int numero) {
+        int result = 0;
+        while (numero < n1 || numero > n2) {
+            System.out.println("Porfavor ingrese una opcion entre: " + n1 + " y " + n2);
+            numero = ValidarEntero(sc);
+        }
+        return numero;
+    }
+
+    public Stack<Integer> LlenarPila(Stack<Integer> p){
+        utilities u =  new utilities();
+        boolean continuar = true;
+        int numero = 0, opt = 0;
+        while (continuar) {
+            System.out.println("Ingrese el numero");
+            numero = u.ValidarEntero(sc);
+            p.push(numero);
+            System.out.println("Desea agregar mas registros 1 = si, 2) no");
+            opt = u.ValidarEntero(sc);
+            opt = u.ValidarRango(1, 2, opt);
+            if (opt == 2) {
+                continuar = false;
+            }
+        }
+        return p;
+    }
+
+    public void MostrarPila(Stack<Integer> p){
+        System.out.println(p);
+    }
 }
